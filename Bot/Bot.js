754e9406d4cfa7ba70a49889d27269b30dc3a7d1
AlikhanFrontDev <alixanhellion@gmail.comgit config --global user.email alixanhellion@gmail.com>
2022-12-16 23:00:31 +0500

    done1

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -1,7 +1,11 @@
 const { Telegraf } = require('telegraf');
-
 const TOKEN = '5852217025:AAF_RvLuREpHblqTOC3BzOGIEdkOW94yAo8'
-
 const bot = new Telegraf(TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
+
+const web_link = 'https://alikhanfrontdev.github.io/telegramWebApp/'
+
+bot.start((ctx) =>
+    ctx.reply('Welcome', {
+        reply_markup: { keyboard: [[{ text: "web_app", web_app: { url: web_link } }]] }
+    }));
 bot.launch();
